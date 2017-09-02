663226ff5f1bf7e81b092239f6ebd3eb0e885ab1
Joseph Hassell <josephh2018@gmail.com>
2017-09-01 23:17:18 -0500

    Cleverbot.io down

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-var cleverbot = require("cleverbot.io"),
+var cleverbot = require("cleverbot.io");
+var config = require("config");
+bot = new cleverbot(config.get("cleverbotIO.apiUser"), config.get("cleverbotIO.apiKey"));
+
+bot.setNick(config.get("cleverbotIO.sessionName"))
+
+console.log()
+
+bot.create(function (err, session) {
+	if(err) {
+		console.error("Error:", err)
+	}
+  bot.ask("Just a small town girl", function (err, response) {
+	  	if(err) {
+			console.error("Error:", err)
+		}
+	  console.log(response); // Will likely be: "Living in a lonely world"
+	});
+});
