1d361dbb2c80648e3b5812453925725d3b2cc0b5
Joseph Hassell <josephh2018@gmail.com>
2017-09-02 00:54:05 -0500

    discouraging test

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-var cleverbot = require("cleverbot.io");
+/*MIT License
+
+Copyright (c) 2017 Joseph Hassell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//var cleverbot = require("cleverbot.io");
 var config = require("config");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var commands = require("./commands/index.js")
 /*bot = new cleverbot(config.get("cleverbotIO.apiUser"), config.get("cleverbotIO.apiKey"));
 
 bot.setNick(config.get("cleverbotIO.sessionName"))
@@ -19,7 +42,7 @@ bot.create(function (err, session) {
 	  console.log(response); // Will likely be: "Living in a lonely world"
 	});
 });*/
-console.log("HII")
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log("Add to discord by going to:", "https://discordapp.com/oauth2/authorize?&client_id=" + client.user.id + "&scope=bot&permissions=0")
@@ -27,9 +50,33 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	
+	if(msg.content === ";;stop") {
+		
+		setTimeout(function() {
+			console.log("hi")
+			commands.otherbots.ams.restart(msg);
+		}, 1000);
+	}
+	
+	if(msg.content.slice(0, config.get("bot.prefix").length) === config.get("bot.prefix")) {
+		console.log("Got Prefix")
+		switch(msg.content.slice(config.get("bot.prefix").length)) {
+			case "ams on":
+				commands.otherbots.ams.isEnabled(true);
+				msg.reply("Will now fight the \"STOP\" command");
+				break;
+			case "ams off":
+				commands.otherbots.ams.isEnabled(false);
+				msg.reply("Will now fight the \"STOP\" command");
+				break;
+			
+		}
+		if (msg.content.slice(config.get("bot.prefix").length) === 'ping') {
+			
+		    msg.reply('Pong!');
+		  }
+	}
 });
 
 
