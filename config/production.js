659c72dbccd94e885ae40f69c4ea102cf9026dfe
Joseph Hassell <josephh2018@gmail.com>
2017-12-29 17:09:44 -0600

    forever

---
+++ config/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"cleverbotIO": {
+		"apiUser": process.env.cleverbotIO_apiUser, //chance this with creds from https://cleverbot.io/keys
+		"apiKey": process.env.cleverbotIO_apiKey //chance this with creds from https://cleverbot.io/keys
+	},
+	"discord": {
+		"token": process.env.discord_token // chance this with creds from https://discordapp.com/developers
+	},
+	"bot": {
+		"prefix": process.env.bot_prefix
+	}
+	
+}
